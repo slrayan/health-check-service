0760b9738352789717e087a547fda4a70e9b4b09
Matteo Russo <matteorusso094@gmail.com>
2023-09-18 11:07:12 +0200

    secondo commit

---
+++ healt-check-service/src/test/java/com/proconsul/HealtCheckServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proconsul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealtCheckServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
